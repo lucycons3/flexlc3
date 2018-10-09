64c5d569004443d5893ff8c92bef457ea872e919
lucycons3 <info@lucycons3.eu>
2018-10-09 12:49:22 +0200

    author.php and category.php added; category-blog.php deleted

--- author.php
+++ author.php
@@ -1,17 +1,46 @@
 <?php get_header(); ?>	
-		<div class="col-md-2 side">
+		<div class="col-2 side">
 					<?php get_sidebar(); ?>
 		</div>
-		<div class="col-md-10">	
-			<?php if (have_posts() ):while ( have_posts() ): the_post(); ?>
-	<!--IMPRIMIR POST-->
-				<h2><?php the_title(); ?></h2>
-				<h4 style="font-size:100%; padding-top:2%;"><?php the_author();?></a> | <?php the_date(); ?></h4>
-				<p><?php the_excerpt();?></p>
-			<?php endwhile; else: ?>
-				<p><?php _e('No hay entradas.'); ?></p>
-			<?php endif; ?>
-	<!--COMENTARIOS-->
-		<?php comments_template(); ?>
+		<div class="col-10" style="padding-left:15%">	
+			<?php
+				// set the "paged" parameter (use 'page' if the query is on a static front page)
+				$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+				
+				// the query
+				$the_query = new WP_Query( array(
+	                'posts_per_page' => 6,
+	                'paged'=>$paged
+	            ) ); 
+				?>
+				<?php if ( $the_query->have_posts() ) : ?>
+				
+				<?php
+				// the loop
+				while ( $the_query->have_posts() ) : $the_query->the_post(); 
+				?>
+				<div class="col-6 entrada" style="background-image:url('<?php echo atrib_imagen_destacada();?>');background-position: center center;background-repeat: no-repeat; background-attachment: fixed;background-size:cover;word-break:normal;">
+					<div class="post">								
+						<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+						<p class="fecha">Publicado el <?php the_time();?> por <a><?php the_author_posts_link(); ?></a> en <?php the_category( ', ' ); ?></p>
+						<p><?php the_excerpt();?></p>
+					</div>
+				</div>
+				<?php endwhile; ?>
+			<div class="">
+				<?php if (function_exists("pagination")) {
+	   		 pagination($the_query->max_num_pages);
+				} ?>
+			</div>
+				
+				<?php 
+				// clean up after the query and pagination
+				wp_reset_postdata(); 
+				?>
+				
+				<?php else:  ?>
+				<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+				<?php endif; ?>
+			</div>
 
 <?php get_footer(); ?>
--- category.php
+++ category.php
@@ -2,24 +2,36 @@
 	
 <!--PÁGINA-->
 		
-		<!-- IMPRIMIRPOST -->
-			<div class="row">	
-				<div class="col-md-12 col-sm-12 col-12">
-					<div class="row">
-						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-						<a href="<?php the_permalink(); ?>"><div class="col-md-4 col-sm-12 col-12 encate fondopre" style="background-image:url('<?php echo atrib_imagen_destacada();?>');">
-							<div class="opac encate fondopre" style="align-items: center;padding-left:5%;padding-right:5%;padding-top:2%;padding-bottom:2%;text-align:left;">
-								<h3><?php the_title();?></h3>
-								<p><?php the_date();?></p>
-							</div>
-						</a>
+				<?php get_header(); ?>
+				<div class="row">
+					<div class="col-2 side">
+						<?php get_sidebar(); ?>
 					</div>
-							<?php endwhile; else: ?>
-							<p><?php _e( 'No hay entradas' ); ?></p>
+					<div class="col-10 ppal">	
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							<div class="col-6 entrada" style="background-image:url('<?php echo atrib_imagen_destacada();?>');background-position: center center;background-repeat: no-repeat; background-attachment: fixed;background-size:cover;word-break:normal;">
+								<div class="post">								
+									<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+									<p class="fecha">Publicado el <?php the_time();?> por <a><?php the_author_posts_link(); ?></a> en <?php the_category( ', ' ); ?></p>
+									<p><?php the_excerpt();?></p>
+								</div>
+							</div>
+							<?php endwhile; ?>
+						<div class="">
+							<?php if (function_exists("pagination")) {
+				   		 pagination($the_query->max_num_pages);
+							} ?>
+						</div>
+							
+							<?php 
+							// clean up after the query and pagination
+							wp_reset_postdata(); 
+							?>
+							
+							<?php else:  ?>
+							<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 							<?php endif; ?>
+					</div>
 				</div>
-			</div>
-		
-		
-<!--FINAL-->
-<?php get_footer();?>
+				<?php get_footer(); ?>
+
