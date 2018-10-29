10a55d8a158c5b40005291fb8c1dd79037bee883
lucycons3 <info@lucycons3.eu>
2018-10-29 17:25:20 +0100

    cambio en taxonomies

--- tag.php
+++ tag.php
@@ -8,7 +8,7 @@
 							<div class="col-6 entrada" style="background-image:url('<?php echo atrib_imagen_destacada();?>');background-position: center center;background-repeat: no-repeat; background-attachment: fixed;background-size:cover;word-break:normal;">
 								<div class="post_flexlc3">								
 									<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-									<p class="fecha">Publicado el <?php the_time('j \d\e F \d\e Y ');?> por <a><?php the_author_posts_link(); ?></a> en <?php the_category( ', ' ); ?></p>
+									<p class="fecha">Publicado el <?php the_time('j \d\e F \d\e Y ');?> por <a><?php the_author_posts_link(); ?></a> en <?php the_taxonomies( ', ' ); ?></p>
 									<p><?php the_excerpt();?></p>
 								</div>
 							</div>
